af129ddb1f54ede6ba8f30846eaa0e05f6bcdb90
Wesam Jabali <wesamjabali@icloud.com>
2020-09-22 14:05:15 -0500

    test

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <HeaderComponent />
+    <Snackbar />
+    <v-main>
+      <v-container>
+        <v-row no-gutters>
+          <v-col cols="12">
+            <router-view />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HeaderComponent from '@/components/HeaderComponent';
+import Snackbar from '@/components/Snackbar';
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+    Snackbar,
+  },
+};
+</script>
